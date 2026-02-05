20582dcb1462930fc1b55c9f9f39344799c49303
Riley L <rlaw.mercer@gmail.com>
2026-02-05 16:19:07 -0500

    fixing volume afterviewinit.

--- frontend/src/app/component/video-player/video-player.ts
+++ frontend/src/app/component/video-player/video-player.ts
@@ -3,24 +3,33 @@ import { Component, Input, ViewChild, ElementRef, inject } from '@angular/core';
 import { ClipService } from '../../services/clip-service';
 import { VolumeService } from '../../services/volume-service';
 import { effect } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
   imports: [],
   templateUrl: './video-player.html',
-  styleUrl: './video-player.scss'
+  styleUrl: './video-player.scss',
 })
-export class VideoPlayer {
+export class VideoPlayer implements AfterViewInit {
   @Input() clipId!: number;
   @Input() autoplay: boolean = false;
   @Input() controls: boolean = true;
 
   volumeService = inject(VolumeService);
-  
+
   @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
-  
+
   private clipService = inject(ClipService);
-  
+
+  constructor() {
+    effect(() => {
+      if (this.videoElement?.nativeElement) {
+        this.videoElement.nativeElement.volume = this.volumeService.volume();
+      }
+    });
+  }
+
   get videoUrl(): string {
     return this.clipService.getVideoUrl(this.clipId);
   }
@@ -28,12 +37,6 @@ export class VideoPlayer {
   ngAfterViewInit() {
     if (this.videoElement) {
       this.videoElement.nativeElement.volume = this.volumeService.volume();
-
-      effect(() => {
-        if (this.videoElement) {
-          this.videoElement.nativeElement.volume = this.volumeService.volume();
-        }
-      })
     }
   }
 
